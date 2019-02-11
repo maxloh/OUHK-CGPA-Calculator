6a005e7bea1723aef1f33bdf0d2b248dce9e1f97
maxloh <35831069+maxloh@users.noreply.github.com>
2019-02-11 20:04:23 +0800

    initial commit

---
+++ OUHK-CGPA-Calculator.js
@@ -0,0 +1,54 @@
+try {
+    var table;
+    document.querySelector('frame[name="TargetContent"]').contentDocument.querySelectorAll('table.PSLEVEL2GRID').forEach(function (element) {
+        let text = element.innerText;
+        if (text.includes('Subj') && text.includes('Catalog') && text.includes('Unit') && text.includes('Grade')) {
+            table = element;
+        }
+    });
+    var results = [];
+    var header = table.querySelectorAll("th");
+
+    table.querySelectorAll("tr").forEach(function (rowElement, rowIndex) {
+        cells = rowElement.querySelectorAll("td");
+        if (cells.length === 0) {
+            return;
+        }
+        results[rowIndex] = {};
+        cells.forEach(function (cellElement, columnIndex) {
+            if (columnIndex <= header.length) {
+                results[rowIndex][header[columnIndex].textContent] = cellElement.textContent.replace(/\n/g, '');
+            }
+        });
+    });
+    var json = {};
+    json.results = results;
+
+    var credit = 0;
+    var gpa = 0;
+    var included = 'Included in CGPA\n';
+    var notIncluded = 'Course Result not released\n';
+    json.results.forEach(function (element) {
+        if (getGPA(element.Grade) !== -1) {
+            included += element.Subj + element.Catalog + '     ' + element.Unit + '     ' + element.Grade + '     ' + getGPA(element.Grade) + '\n';
+            credit += Number(element.Unit);
+            gpa += element.Unit * getGPA(element.Grade);
+        } else {
+            notIncluded += element.Subj + element.Catalog + '\n';
+        }
+    });
+    alert('CGPA: ' + (gpa / credit) + '\n\n' + notIncluded + '\n' + included + '\n' + 'OUHK CGPA Calculator by Max Loh');
+
+    function getGPA(grade) {
+        if (grade === 'A') return 4.0;
+        if (grade === 'A-') return 3.7;
+        if (grade === 'B+') return 3.3;
+        if (grade === 'B') return 3.0;
+        if (grade === 'B-') return 2.7;
+        if (grade === 'C+') return 2.3;
+        if (grade === 'C') return 2.0;
+        return -1;
+    }
+} catch (e) {
+    alert('Please press "view all terms" button in "Academic Record" page and try run this script again')
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# OUHK CGPA 計算器
+
+頭盔：最好自己驗算一次
+
+## 點用？
+
+### Chrome
+
+- 登入myouhk ➜ 你讀緊個Programme ➜ Academic Record ➜ view all terms按鈕
+- 之後嘅步驟有啲難
+  - 按<kbd>Control</kbd>+<kbd>Shift</kbd>+<kbd>J</kbd>
+  - 將[OUHK-CGPA-Calculator.js](https://raw.githubusercontent.com/maxloh/OUHK-CGPA-Calculator/master/OUHK-CGPA-Calculator.js)嘅內容貼去控制檯到
+  - 按<kbd>Enter</kbd>
