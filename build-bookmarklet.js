eeaff18ce2ab5a892ecaf443a3acc0b5f9c3a8df
maxloh <35831069+maxloh@users.noreply.github.com>
2019-02-16 03:51:06 +0800

    Format build-bookmarklet.js, change some code

--- build-bookmarklet.js
+++ build-bookmarklet.js
@@ -2,23 +2,13 @@
 Building steps of the JavaScript bookmarklet
 1. Install Node.js
 2. Run 'npm install google-closure-compiler'
-2. Run 'node ".\build-bookmarklet.js"'
+2. Run 'node .\build-bookmarklet.js'
 3. The bookmarklet is now in the "bookmarklet.js" file
 */
 
 const ClosureCompiler = require('google-closure-compiler').jsCompiler;
 const fs = require('fs');
 
-fs.readFile('OUHK-CGPA-Calculator.js', (err, data) => {
-    if (err) throw err;
-    new ClosureCompiler({
-        compilation_level: 'WHITESPACE_ONLY'
-    }).run([{
-        src: data.toString(),
-        sourceMap: null // optional input source map
-    }], (exitCode, stdOut, stdErr) => {
-        fs.writeFile('bookmarklet.js', 'javascript:(function(){' + stdOut[0].src.replace(/\n/g, '') + '})();', (err) => {
-            if (err) throw err;
-        });
-    });
+new ClosureCompiler({ js: 'OUHK-CGPA-Calculator.js', compilation_level: 'WHITESPACE_ONLY', output_wrapper: 'javascript:(function(){%output%})();' }).run([], (exitCode, stdOut, stdErr) => {
+    fs.writeFile('bookmarklet.js', stdOut[0].src.replace(/\n/g, ''), (err) => { if (err) throw err; });
 });
