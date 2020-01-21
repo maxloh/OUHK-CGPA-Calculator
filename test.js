ac3a1b53bfbbab60c9659bdfeecbe981b654d762
maxloh-study <47226658+maxloh-study@users.noreply.github.com>
2020-01-22 04:14:57 +0800

    temp4

---
+++ test.js
@@ -0,0 +1,120 @@
+let a = [
+    {
+        "Subj": "COMP",
+        "Catalog": "S202F",
+        "Unit": "10.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S263F",
+        "Unit": "10.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S209F",
+        "Unit": "10.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S266F",
+        "Unit": "10.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S267F",
+        "Unit": "10.00",
+        "Grade": "B+"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S268F",
+        "Unit": "5.00",
+        "Grade": "B+"
+    }
+];
+
+let b = [
+    {
+        "Subj": "COMP",
+        "Catalog": "S202F",
+        "Unit": "10.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S263F",
+        "Unit": "10.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S209F",
+        "Unit": "10.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S266F",
+        "Unit": "5.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S267F",
+        "Unit": "5.00",
+        "Grade": "B+"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S268F",
+        "Unit": "10.00",
+        "Grade": "B+"
+    }
+];
+
+let c = [
+    {
+        "Subj": "COMP",
+        "Catalog": "S202F",
+        "Unit": "10.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S263F",
+        "Unit": "10.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S209F",
+        "Unit": "10.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S266F",
+        "Unit": "5.00",
+        "Grade": "A"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S267F",
+        "Unit": "10.00",
+        "Grade": "B+"
+    },
+    {
+        "Subj": "COMP",
+        "Catalog": "S268F",
+        "Unit": "5.00",
+        "Grade": "B+"
+    }
+];
+
+getWGPA(a, false);
+getWGPA(a, false);
+getWGPA(a, false);
